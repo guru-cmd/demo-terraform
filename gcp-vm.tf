2b2607f286162204304387ca65e9d0c1aa1210e7
Gurudatta Panda <62826602+guru-cmd@users.noreply.github.com>
2023-06-20 16:27:03 +0530

    Update gcp-vm.tf
    updated

--- gcp-vm.tf
+++ gcp-vm.tf
@@ -6,7 +6,7 @@ resource google_compute_instance "gcp-vm-tf-cloud" {
   name = "tf-cloud-exp"
   zone = "us-west1-a"
   machine_type = "f1-micro"
-  tag = ["testing", "tf-cloud"]
+  tags = ["testing", "tf-cloud"]
   boot_disk {
     initialize_params {
      image = "debian-cloud/debian-11"
